9062f9d9c5cf7cf1ece13403c074e8a9ed598146
gece901 <ffkiran615@gmail.com>
2024-10-05 14:06:21 +0200

    Update ready.js

--- AkiCode_V14_Ticket_Bot/events/ready.js
+++ AkiCode_V14_Ticket_Bot/events/ready.js
@@ -1,12 +1,12 @@
 const { ActivityType } = require('discord.js');
 
 module.exports = {
-    name: 'ready',
+    name: 'GeceTicket',
     once: true,
     execute(client) {
         console.log(`${client.user.tag} olarak giriş yapıldı!`);
         client.user.setPresence({
-            activities: [{ name: 'AkiCode & ParsherCode', type: ActivityType.Streaming, url: 'https://www.twitch.tv/akicode' }],
+            activities: [{ name: 'GECE', type: ActivityType.playing, url: 'GECE' }],
             status: 'online',
         });
     },
